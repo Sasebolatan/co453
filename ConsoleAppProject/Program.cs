7b910fee48afe8e14e4757c52934a5baf0b8f759
Sasebolatan <91736171+Sasebolatan@users.noreply.github.com>
2022-09-07 17:03:46 +0100

    update

--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -18,39 +18,70 @@ namespace ConsoleAppProject
     /// This Project has been modified by:
     /// Derek Peacock 05/02/2022
     /// </summary>
+
     public static class Program
     {
-        private static DistanceConverter converter = new DistanceConverter();
+        public static int choice;
 
-        private static BMI calculator = new BMI();
+        public static BMI BMI
+        {
+            get => default;
+            set
+            {
+            }
+        }
 
+        public static StudentGrades StudentGrades
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+     
         public static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.Yellow;
 
+
+
+
+
+
             Console.WriteLine();
             Console.WriteLine(" =================================================");
             Console.WriteLine("    BNU CO453 Applications Programming 2021-2022! ");
-            Console.WriteLine("                    samuel                  ");
+            Console.WriteLine("                       samuel                     ");
             Console.WriteLine(" =================================================");
             Console.WriteLine();
 
 
             Console.WriteLine("1. Distance Converter");
             Console.WriteLine("2. BMI Calculator");
+            Console.WriteLine("3. Student Marks");
+            Console.WriteLine("4. news app");
 
             Console.WriteLine("Please enter your choice of App  >  ");
             string choice = Console.ReadLine();
 
             if (choice == "1")
             {
-                converter.ConvertDistance();
+                DistanceConverter converter = new DistanceConverter();
+                converter.Run();
             }
             else if (choice == "2")
             {
-                calculator.CalculateIndex();
+                BMI calculator = new BMI();
+                calculator.Run();
 
             }
+            else if (choice == "3")
+            {
+                StudentGrades grades = new StudentGrades();
+                grades.Run();
+            }
+
             else Console.WriteLine("Invalid Choice !");
 
 
